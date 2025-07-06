456499e6f9b0bf67bd074c7e747f607d3a6d734a
CJcode6754 <ceejayibabiosa@gmail.com>
2025-07-06 20:38:12 +0800

    modified

--- server/src/server.js
+++ server/src/server.js
@@ -24,10 +24,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client", "dist", "index.html"))
     })
 }
 
